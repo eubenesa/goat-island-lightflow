236e4cfdcf917ad89386ccfd281b516ffeacbd9a
Eubene Sa <eubene.sa@gmail.com>
2014-08-27 10:50:51 -0700

    ready for models

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,8 @@
 // For more information see: http://emberjs.com/guides/routing/
+App.Router.reopen({
+  location: 'auto',
+  rootURL: '/questions'
+})
 
-App.Router.map(function() {
-  // this.resource('posts');
+App.Router.map(function() {  
 });
--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,9 +1,9 @@
-App.ApplicationStore = DS.Store.extend({
-
+DS.RESTAdapter.reopen({
+  namespace: 'api/v1'
 });
+
+App.ApplicationStore = DS.Store.extend({});
 
 // Override the default adapter with the `DS.ActiveModelAdapter` which
 // is built to work nicely with the ActiveModel::Serializers gem.
-App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
-
-});
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend({});
