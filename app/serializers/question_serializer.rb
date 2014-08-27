0232e4cb395096cc381fdb7181956aea54497dfb
Eubene Sa <eubene.sa@gmail.com>
2014-08-27 16:24:47 -0700

    serializers done for now, there is an issue when pushing up to heroku

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,6 +1,7 @@
 class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :subject, :content, :category, :upvotes, :updated_at
 
+  belongs_to :user
   has_many :answers
 
   url :question  
