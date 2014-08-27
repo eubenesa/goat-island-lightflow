8d132bc134750c1a8e3d5143472005198aa25ea0
Eubene Sa <eubene.sa@gmail.com>
2014-08-27 16:05:16 -0700

    took out belongs_to user from serializers

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,7 +1,6 @@
 class AnswerSerializer < ActiveModel::Serializer
   attributes :id, :content, :upvotes, :updated_at
 
-  belongs_to :user
   belongs_to :question
 
   url [:question, :answer]
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,7 +1,6 @@
 class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :subject, :content, :category, :upvotes, :updated_at
 
-  belongs_to :user
   has_many :answers
 
   url :question  
