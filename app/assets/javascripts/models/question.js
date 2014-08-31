4c8572812a405617b155b0c1ecc80c10c716d640
Eubene Sa <eubene.sa@gmail.com>
2014-08-31 16:17:45 -0700

    db.yml update with postgres.app, number of upvotes in questions index

--- app/assets/javascripts/models/question.js
+++ app/assets/javascripts/models/question.js
@@ -8,6 +8,10 @@ App.Question = DS.Model.extend({
   user: DS.belongsTo('user', { async: true }),
   // answers: DS.hasMany('answer'),
 
+  upvotesLength: function() {
+    return this.get('upvotes').length;
+  }.property('upvotes'),
+
   formattedUpdatedAt: function() {
     return moment(this.get('updated_at')).fromNow();
   }.property('updated_at')
