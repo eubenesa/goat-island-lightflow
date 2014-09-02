ef32c45baf29c4da4e4811157013987c47c6a696
Eubene Sa <eubene.sa@gmail.com>
2014-09-02 11:32:00 -0700

    questions and answers rails controllers updated

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::AnswersController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Question.find(params[:question_id]).answers
+  end
+
+  def create
+  end
+
+  # def show
+  #   respond_with Question.find(params[:question_id]).answers.find(params[:id])
+  # end
+
+  # def update
+  #   answer = Question.find(params[:question_id]).answers.find(params[:id])
+  #   respond_with answer.update(answer_params)
+  # end
+
+  # def destroy
+  #   answer = Question.find(params[:question_id]).answers.find(params[:id])
+  #   respond_with answer.destroy
+  # end
+
+  # def answer_params
+  #   params.require(:answer).permit(:content)
+  # end
 end
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -6,13 +6,9 @@ class Api::V1::QuestionsController < ApplicationController
   end
 
   def create
-    # params = question_params
-    # params[:user_id] = session[:user_id]
-    # binding.pry
     respond_with :api, :v1, Question.create(
       subject: params[:question][:subject],
       content: params[:question][:content],
-      # category: params[:question][:category],
       user_id: current_user.id
     )
   end
@@ -21,20 +17,19 @@ class Api::V1::QuestionsController < ApplicationController
     respond_with Question.find(params[:id])
   end
 
-  def update
-    question = Question.find(params[:id])
-    # respond_with question.update(question_params)
-  end
+  # def update
+  #   question = Question.find(params[:id])
+  #   respond_with question.update(question_params)
+  # end
 
-  def destroy
-    question = Question.find(params[:id])
-    respond_with question.destroy
-  end
+  # def destroy
+  #   question = Question.find(params[:id])
+  #   respond_with question.destroy
+  # end
 
   # private
 
   # def question_params
   #   params.require(:question).permit(:subject, :content, :category);
   # end
-
 end
