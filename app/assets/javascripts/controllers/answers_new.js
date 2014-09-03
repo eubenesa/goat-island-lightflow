f45e093579c2001e78aa89d0c5c2749d6c2d36f0
Eubene Sa <eubene.sa@gmail.com>
2014-09-03 15:51:12 -0700

    getting some help for answers.new

--- app/assets/javascripts/controllers/answers_new.js
+++ app/assets/javascripts/controllers/answers_new.js
@@ -1,11 +1,13 @@
 App.AnswersNewController = Ember.Controller.extend({
   actions: {
-    createAnswer: function(params) {
+    createAnswer: function() {
       var self = this;
-      var answer = this.store.createRecord('answer', {
+
+      var answer = this.store.createRecord('answer', function(params) {
         content: this.get('fields.content'),
-        question: this.store.find('question', params.question_id)
-      }
+        question_id: this.modelFor('question').get('question_id')
+      });
+
       answer.save().then(function() {
         self.transitionToRoute('answers.index');
       });
